cc7111e9d736446aeabe9709bb80456442a43e59
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-10 14:36:43 +0530

    feat: render navbar, footer, landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
+import { LandingPage } from "pages/";
+import { Footer, Navbar } from "components";
+
 function App() {
 	return (
-		<div class="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-			<div>
-				<span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-					<svg
-						class="h-6 w-6 text-white"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						aria-hidden="true"
-					></svg>
-				</span>
+		<div className="bg-gray-100 text-gray-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl">
+			<Navbar />
+			<div className="main h-full flex flex-col items-center justify-center min-h-[90vh]">
+				<LandingPage />
 			</div>
-			<h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
-				Writes Upside-Down
-			</h3>
-			<p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-				The Zero Gravity Pen can be used to write in any orientation,
-				including upside-down. It even works in outer space.
-			</p>
+			<Footer />
 		</div>
 	);
 }
