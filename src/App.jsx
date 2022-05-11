434016928ed01a99a6572f8d78cb00b0b9614fb2
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-11 19:23:05 +0530

    feat: added navroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { LandingPage } from "features/";
+import React from "react";
 import { Footer, Navbar } from "components";
+import { NavRoutes } from "routes/NavRoutes";
 
 function App() {
 	return (
-		<div className="bg-gray-100 text-gray-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl">
+		<div className="bg-gray-100 text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl">
 			<Navbar />
 			<div className="main h-full flex flex-col items-center justify-center min-h-[90vh]">
-				<LandingPage />
+				<NavRoutes />
 			</div>
 			<Footer />
 		</div>
