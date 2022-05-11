a05f2f36c37ee86c6e961c7cf6a86c61c4dbc01e
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-11 19:33:42 +0530

    feat: barrel export

--- src/features/index.js
+++ src/features/index.js
@@ -1,1 +1,5 @@
 export { LandingPage } from "./LandingPage";
+export { Login } from "./Auth/Login";
+export { SignUp } from "./Auth/Signup";
+export * from "./Auth/authSlice";
+export { Home } from "./Home";
