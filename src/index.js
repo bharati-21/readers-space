50a81b8e95dd8b76aa00873b8dd59cf496e9c202
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-11 19:44:14 +0530

    feat: wrapped provider and store. added portal

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "store";
+import ToastPortal from "ToastPortal";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+				<ToastPortal />
+			</Provider>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
