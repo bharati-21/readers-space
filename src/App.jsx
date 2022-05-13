fc7952d03e033c7f196a8bd41e3d129662abb735
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-13 20:41:06 +0530

    feat: hide navbar from login, signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Footer, Navbar } from "components";
 import { NavRoutes } from "routes/NavRoutes";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { getAuthState } from "features";
 
 function App() {
 	const location = useLocation();
-	const { isAuth } = useSelector(getAuthState);
 
 	return (
-		<div className="bg-gray-100 text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl transition-all ease-in">
-			<Navbar />
+		<div className="bg-gray-200 text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl transition-all ease-in">
+			{location.pathname === "/signup" ||
+			location.pathname === "/login" ? null : (
+				<Navbar />
+			)}
 			<div className="main h-full flex flex-col items-center justify-center min-h-[100vh]">
 				<NavRoutes />
 			</div>
-			{!isAuth && location.pathname === "/" ? <Footer /> : null}
+			{location.pathname === "/" ? <Footer /> : null}
 		</div>
 	);
 }
