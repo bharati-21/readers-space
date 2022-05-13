8d1c15178fc653778e86e9bb4c36d01da6a2311f
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-13 17:30:25 +0530

    feat: edit home navroute

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
 
-import { getAuthState, Home, LandingPage, Login, SignUp } from "features";
+import { getAuthState, Login, SignUp } from "features";
+import { Home, LandingPage } from "pages";
 import { useSelector } from "react-redux";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 
 const NavRoutes = () => {
-	const { isAuth } = useSelector(getAuthState);
 
 	return (
 		<Routes>
-			{!isAuth && <Route path="/" element={<LandingPage />} />}
+			<Route path="/" element={<LandingPage />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<SignUp />} />
 			<Route path="/mockman" element={<Mockman />} />
 			<Route path="/" element={<ProtectedRoutes />}>
-				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
 			</Route>
 		</Routes>
 	);
