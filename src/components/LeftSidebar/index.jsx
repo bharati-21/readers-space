63a83e227b3c2d2791b510f38a4e572875f7a652
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-13 17:24:18 +0530

    feat: loader, sidebar, rightsidebar components

---
+++ src/components/LeftSidebar/index.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Home, Explore, Bookmark, AccountCircle, Add } from "@mui/icons-material";
+import { v4 as uuid } from "uuid";
+import { NewPostModal } from "features";
+
+const LeftSidebar = () => {
+	const getActiveClassName = ({ isActive }) =>
+		isActive ? `active-link link` : `link`;
+
+    const [showModal, setShowModal] = useState(false);
+    
+    useEffect(() => {
+        if(showModal) {
+            document.body.style.overflow = 'hidden';
+        }
+        else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [showModal]);
+
+	const sidebarLinks = [
+		{
+			id: uuid(),
+			link: "Home",
+			icon: <Home />,
+			to: "/",
+		},
+		{
+			id: uuid(),
+			link: "Explore",
+			icon: <Explore />,
+			to: "/explore",
+		},
+		{
+			id: uuid(),
+			link: "Bookmarks",
+			icon: <Bookmark />,
+			to: "/bookmarks",
+		},
+		{
+			id: uuid(),
+			link: "Profile",
+			icon: <AccountCircle />,
+			to: "/profile",
+		},
+	];
+
+	const getMappedLinks = (linkFor = "footer") =>
+		sidebarLinks.map(({ to, id, link, icon }) => (
+			<li key={id}>
+				<NavLink to={to} className={getActiveClassName}>
+					{icon} {linkFor === "sidebar" ? link : null}
+				</NavLink>
+			</li>
+		));
+
+    const handleShowModalDialog = ( modalVisibilityState) => {
+        setShowModal(modalVisibilityState);
+    }
+
+	return (
+		<>
+            { showModal ? <NewPostModal handleShowModalDialog={handleShowModalDialog} /> : null }
+			<aside className="sidebar-nav py-8 p-4 pr-8 hidden md:flex flex-col justify-start items-center h-full w-full max-w-[180px]">
+				<div className="flex fixed flex-col justify-between items-center">
+					<ul className="flex list-none flex-col gap-4 justify-center w-full">
+						{getMappedLinks("sidebar")}
+					</ul>
+					<button className="btn-primary max-w-[160px] w-full py-1.5 fixed bottom-10" onClick={e => handleShowModalDialog(true)}>
+						Create New Post
+					</button>
+				</div>
+			</aside>
+			<ul className="list-none fixed bottom-0 md:hidden flex flex-row gap-4 py-4 dark:bg-slate-800 px-8 justify-between items-center w-full left-0 right-0 flex-wrap z-[5]">
+				{getMappedLinks()}
+			</ul>
+            <button className="btn-primary md:hidden flex flex-col justify-center items-center w-10 h-10 py-1.5 fixed right-[2rem] bottom-[5rem] z-[5] rounded-[50%]" onClick={e => handleShowModalDialog(true)}>
+				<Add />
+			</button>
+		</>
+	);
+};
+
+export { LeftSidebar };
---
+++ src/components/RightSidebar/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+
+const RightSidebar = () => {
+	return (
+		<aside className="p-4 py-8 pl-8 hidden lg:flex flex-col items-center w-full">
+			<div className=" flex fixed flex-col gap-6 justify-between items-start w-full max-w-[250px]">
+				<h3 className="text-xl">Who to Follow</h3>
+				<ul className="flex list-none flex-col gap-4 justify-start w-full bg-slate-800 p-3 rounded-sm">
+					<li className="flex flex-row items-center justify-between w-full gap-3">
+						<div className="user-info gap-2 flex flex-row items-start justfiy-between">
+							<img
+								className="inline-block h-7 w-7 rounded-full ring-2 ring-sky-500"
+								src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+								alt="John Doe Profile Image"
+							/>
+							<div className="username-info flex flex-col justify-between items-center">
+								<p className="text-xs break-before-all break-all">John Doe</p>
+								<p className="text-xs break-before-all break-all">@johndoe</p>
+							</div>
+						</div>
+						<button className="btn-primary text-xs py-1 px-2">
+							Follow
+						</button>
+					</li>
+                    <li className="flex flex-row items-center justify-between w-full gap-3">
+						<div className="user-info gap-2 flex flex-row items-start justfiy-between overflow-ellipsis">
+							<img
+								className="inline-block h-7 w-7 rounded-full ring-2 ring-sky-500 shrink-0 object-cover"
+								src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+								alt="Jane Doe Profile Image"
+							/>
+							<div className="username-info flex flex-col justify-between items-center flex-wrap">
+								<p className="text-xs break-before-all break-all">Jane Doe</p>
+								<p className="text-xs break-before-all break-all">@janedoe</p>
+							</div>
+						</div>
+						<button className="btn-primary text-xs py-1 px-2">
+							Follow
+						</button>
+					</li>
+                    <li className="flex flex-row items-center justify-between w-full gap-3">
+						<div className="user-info gap-2 flex flex-row items-start justfiy-between">
+							<img
+								className="inline-block h-7 w-7 rounded-full ring-2 ring-sky-500 shrink-0 object-cover"
+								src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+								alt="You know who Profile Image"
+							/>
+							<div className="username-info flex flex-col justify-between align-center">
+								<p className="text-xs break-before-all break-all">You know who</p>
+								<p className="text-xs break-before-all break-all">@youknowho</p>
+							</div>
+						</div>
+						<button className="btn-primary text-xs py-1 px-2">
+							Follow
+						</button>
+					</li>
+				</ul>
+			</div>
+		</aside>
+	);
+};
+
+export { RightSidebar };
