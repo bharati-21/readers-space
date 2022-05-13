ea29c05f188dad9573402a4a73099f563acf1bc7
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-13 20:43:32 +0530

    styles: light theme suggestion text color

--- src/components/RightSidebar/index.jsx
+++ src/components/RightSidebar/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 const RightSidebar = () => {
 	return (
 		<aside className="p-4 py-8 pl-8 hidden lg:flex flex-col items-center w-full">
 			<div className=" flex fixed flex-col gap-6 justify-between items-start w-full max-w-[250px]">
 				<h3 className="text-xl">Who to Follow</h3>
-				<ul className="flex list-none flex-col gap-4 justify-start w-full bg-slate-800 p-3 rounded-sm">
+				<ul className="flex list-none flex-col gap-4 justify-start w-full bg-slate-800 p-3 rounded-sm text-gray-100">
 					<li className="flex flex-row items-center justify-between w-full gap-3">
 						<div className="user-info gap-2 flex flex-row items-start justfiy-between">
 							<img
@@ -15,15 +14,19 @@ const RightSidebar = () => {
 								alt="John Doe Profile Image"
 							/>
 							<div className="username-info flex flex-col justify-between items-center">
-								<p className="text-xs break-before-all break-all">John Doe</p>
-								<p className="text-xs break-before-all break-all">@johndoe</p>
+								<p className="text-xs break-before-all break-all">
+									John Doe
+								</p>
+								<p className="text-xs break-before-all break-all">
+									@johndoe
+								</p>
 							</div>
 						</div>
 						<button className="btn-primary text-xs py-1 px-2">
 							Follow
 						</button>
 					</li>
-                    <li className="flex flex-row items-center justify-between w-full gap-3">
+					<li className="flex flex-row items-center justify-between w-full gap-3">
 						<div className="user-info gap-2 flex flex-row items-start justfiy-between overflow-ellipsis">
 							<img
 								className="inline-block h-7 w-7 rounded-full ring-2 ring-sky-500 shrink-0 object-cover"
@@ -31,15 +34,19 @@ const RightSidebar = () => {
 								alt="Jane Doe Profile Image"
 							/>
 							<div className="username-info flex flex-col justify-between items-center flex-wrap">
-								<p className="text-xs break-before-all break-all">Jane Doe</p>
-								<p className="text-xs break-before-all break-all">@janedoe</p>
+								<p className="text-xs break-before-all break-all">
+									Jane Doe
+								</p>
+								<p className="text-xs break-before-all break-all">
+									@janedoe
+								</p>
 							</div>
 						</div>
 						<button className="btn-primary text-xs py-1 px-2">
 							Follow
 						</button>
 					</li>
-                    <li className="flex flex-row items-center justify-between w-full gap-3">
+					<li className="flex flex-row items-center justify-between w-full gap-3">
 						<div className="user-info gap-2 flex flex-row items-start justfiy-between">
 							<img
 								className="inline-block h-7 w-7 rounded-full ring-2 ring-sky-500 shrink-0 object-cover"
@@ -47,8 +54,12 @@ const RightSidebar = () => {
 								alt="You know who Profile Image"
 							/>
 							<div className="username-info flex flex-col justify-between align-center">
-								<p className="text-xs break-before-all break-all">You know who</p>
-								<p className="text-xs break-before-all break-all">@youknowho</p>
+								<p className="text-xs break-before-all break-all">
+									You know who
+								</p>
+								<p className="text-xs break-before-all break-all">
+									@youknowho
+								</p>
 							</div>
 						</div>
 						<button className="btn-primary text-xs py-1 px-2">
