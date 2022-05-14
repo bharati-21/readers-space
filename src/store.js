1946c4dabfdf3bad0f1bbfc4682b1579a717a5d6
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-14 12:52:11 +0530

    feat: post modal reducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, postsReducer } from "features/";
+import { authReducer, postModalReducer, postsReducer } from "features/";
 
 const store = configureStore({
-	reducer: { auth: authReducer, posts: postsReducer },
+	reducer: {
+		auth: authReducer,
+		posts: postsReducer,
+		postModal: postModalReducer,
+	},
 });
 
 export default store;
