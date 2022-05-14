66c153852481fbfe1ed6c43af3d03d981aeda779
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-14 22:21:49 +0530

    feat: added sidebar common components

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -3,13 +3,18 @@ import { Outlet, useLocation, Navigate } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 import { getAuthState } from "features";
+import { LeftSidebar, RightSidebar } from "components";
 
 const ProtectedRoutes = () => {
 	const { isAuth } = useSelector(getAuthState);
 	const location = useLocation();
 
 	return isAuth ? (
-		<Outlet />
+		<main className="min-h-[100vh] w-full px-8 sm:px-6 grid grid-cols-1 md:grid-container">
+			<LeftSidebar />
+			<Outlet />
+			<RightSidebar />
+		</main>
 	) : (
 		<Navigate to="/login" state={{ from: location.pathname }} replace />
 	);
