e8dd5aeb91cc69b860b237104b83c1b7eb576983
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-15 23:47:38 +0530

    fix: gap issue

--- src/features/Post/components/CommentsContainer/index.jsx
+++ src/features/Post/components/CommentsContainer/index.jsx
@@ -7,7 +7,7 @@ import { CommentsList } from "./CommentsList";
 const CommentsContainer = () => {
 	const post = useOutletContext();
 	return (
-		<div className="comments-container">
+		<div className="flex flex-col gap-4">
 			<CommentInput postId={post._id} />
 			<CommentsList post={post} />
 		</div>
