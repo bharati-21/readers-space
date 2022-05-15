c71d0cf09f4340623e161cfbdb1097ce7f119bf6
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-15 23:43:16 +0530

    refactor: footer component width

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="dark:bg-slate-800 bg-gray-100">
+		<footer className="dark:bg-slate-800 bg-gray-100 w-full">
 			<div className="max-w-8xl mx-auto p-4 sm:px-6 lg:p-6 flex flex-col flex-align-center flex-justify-center gap-2">
 				<p className="text-center flex flex-row items-center justify-center author gap-1.5 flex-wrap text-lg">
 					<i className="fa-solid fa-code"></i> by
