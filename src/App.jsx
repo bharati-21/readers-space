50ed6a82677a1da14fabb052d4895fdffea71b09
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-15 23:25:39 +0530

    feat: added child routes using Outlet
    Render components only in required location using child routes and outlet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import { Footer, Navbar } from "components";
 import { NavRoutes } from "routes/NavRoutes";
-import { useLocation } from "react-router-dom";
 
-function App() {
-	const location = useLocation();
-
+const App = () => {
 	return (
-		<div className="bg-gray-200 text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl transition-all ease-in">
-			{location.pathname === "/signup" ||
-			location.pathname === "/login" ? null : (
-				<Navbar />
-			)}
-			<div className="main h-full flex flex-col items-center justify-center min-h-[100vh]">
-				<NavRoutes />
-			</div>
-			{location.pathname === "/" ? <Footer /> : null}
+		<div className="bg-gray-200 min-h-[100vh] flex-col flex justify-center w-full items-center text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl transition-all ease-in">
+			<NavRoutes />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet, useLocation, Navigate } from "react-router-dom";
-
 import { useSelector } from "react-redux";
+
 import { getAuthState } from "features";
 import { LeftSidebar, RightSidebar } from "components";
 
