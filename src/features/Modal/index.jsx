d7a32a55674395c152e2a333fb343c046f22886f
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-18 10:35:31 +0530

    feat: conditionally render modal content

--- src/features/Modal/index.jsx
+++ src/features/Modal/index.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { getModalState } from "features";
+import { getModalState, PostContainer } from "features";
 import { useOnOutsideClick } from "hooks";
-import { PostModal } from "components";
+import { EditProfileForm, PostModal } from "components";
+import { useLocation } from "react-router-dom";
+import { editModalVisibility } from "./modalSlice";
 
 const Modal = () => {
 	const modalContainerRef = useRef();
 
 	const modal = useSelector(getModalState);
 	const { modalVisibilityState, modalChildren } = modal;
+	const location = useLocation();
 
 	useOnOutsideClick(modalContainerRef);
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		if (modalVisibilityState) {
@@ -21,6 +25,15 @@ const Modal = () => {
 		}
 	}, [modalVisibilityState]);
 
+	useEffect(() => {
+		dispatch(
+			editModalVisibility({
+				modalVisibilityState: false,
+				modalChildren: null,
+			})
+		);
+	}, [location.pathname]);
+
 	return !modalVisibilityState ? null : (
 		<div
 			className="absolute z-10 h-screen transition-all ease-in"
@@ -28,10 +41,17 @@ const Modal = () => {
 		>
 			<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-			<div className="fixed z-10 inset-0 overflow-y-auto h-full w-full flex flex-col justify-start items-center p-4 my-8">
-				{modalChildren === "POST_MODAL" ? (
-					<PostModal modalContainerRef={modalContainerRef} />
-				) : null}
+			<div className="fixed z-10 inset-0 overflow-y-auto h-full w-full flex flex-col justify-start items-center p-8">
+				<div
+					className="rounded-lg shadow-xl transition-all align-middle max-w-lg h-max w-full relative"
+					ref={modalContainerRef}
+				>
+					{modalChildren === "POST_MODAL" ? (
+						<PostContainer container="modal" />
+					) : modalChildren === "EDIT_PROFILE" ? (
+						<EditProfileForm />
+					) : null}
+				</div>
 			</div>
 		</div>
 	);
