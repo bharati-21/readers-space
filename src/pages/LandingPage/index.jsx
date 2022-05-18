ac66dff19c0f6b0e7e18ac31120fc7a56d1e74f4
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-18 10:54:45 +0530

    fix: add missing imports

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import heroImage from "images/hero-image.svg";
 import logoImage from "images/readers-space-logo.png";
