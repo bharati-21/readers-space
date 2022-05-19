d5067fa0a79907ba1771f4f261fb5e5b4bfa2733
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 14:06:30 +0530

    feat: suggested users, users list component

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,5 @@ export { RightSidebar } from "./RightSidebar";
 export { Loader } from "./Loader";
 export { CircleProgressBar } from "./CirlceProgress";
 export { EditProfileForm } from "./EditProfileForm";
+export { SuggestedUsers } from "./SuggestedUsers";
+export { UsersList } from "./UsersList";
