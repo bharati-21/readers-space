7a5e743a2083746be1abf659254f701418e2d146
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 22:21:22 +0530

    refactor: formatting

--- src/features/Post/components/PostsList/index.jsx
+++ src/features/Post/components/PostsList/index.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
-import { PostItem, getPostsState } from "features";
+import { PostItem } from "features";
 import { useInfiniteScroll } from "hooks";
 import inifnityLoading from "images/loading-infinity.svg";
-import { getSortedPosts } from "utils";
-import { useLocation } from "react-router-dom";
 
 const PostsList = ({ posts }) => {
 	const { pageNumber, loading, lastElementReference, hasMorePosts } =
 		useInfiniteScroll(posts);
-	const location = useLocation();
-	const { sortBy } = useSelector(getPostsState);
 
-	const sortedPosts =
-		location.pathname === "/home"
-			? getSortedPosts(posts, sortBy)
-			: getSortedPosts(posts, "LATEST");
-
-	const postsToDisplay = sortedPosts?.slice(0, pageNumber * 3);
+	const postsToDisplay = posts?.slice(0, pageNumber * 3);
 	return (
 		<div className="w-full flex justify-center items-center gap-8 my-12 flex-col">
 			{postsToDisplay?.map((post) => (
