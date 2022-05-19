4ba2ca5cbf6b43b03f9e753b19ce70863aa86a9a
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 14:13:20 +0530

    feat: added unfollowed user posts

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useDocumentTitle, useToast } from "hooks";
+import {
+	getPosts,
+	getPostsState,
+	getUsersState,
+	PostContainer,
+	PostsList,
+	getAuthState,
+} from "features";
+import errorImage from "images/error-image.svg";
+import caughtUp from "images/all-caught-up.svg";
+import { Loader } from "components";
+import { getUnfollowedUsers, getUnfollowedUsersPosts } from "utils";
 
 const Explore = () => {
-	return <div>index</div>;
+	const {
+		authToken,
+		authUser: { following, username },
+	} = useSelector(getAuthState);
+	const dispatch = useDispatch();
+	const { posts, postsLoading, postsError } = useSelector(getPostsState);
+	const { users, usersLoading, usersError } = useSelector(getUsersState);
+	const { showToast } = useToast();
+
+	const { setDocumentTitle } = useDocumentTitle();
+
+	useEffect(() => {
+		setDocumentTitle("ReadersSpace | Explore");
+		(async () => {
+			try {
+				const postsDispatchResponse = await dispatch(
+					getPosts(authToken)
+				);
+				if (postsDispatchResponse.error)
+					throw new Error(
+						"Could not load users posts. Try again later"
+					);
+			} catch (error) {
+				showToast(error.message, "error");
+			}
+		})();
+	}, []);
+
+	const unfollowedUsersPosts = getUnfollowedUsersPosts(
+		users,
+		posts,
+		username
+	);
+
+	return postsLoading || usersLoading ? (
+		<Loader />
+	) : postsError || usersError ? (
+		<section className="home p-8 px-0 md:px-8 border-0 md:border-l lg:border-r border-x-sky-400 flex flex-col items-center justify-start w-full">
+			<div className="max-w-[1080px] w-full flex flex-col justify-start items-center">
+				<h3 className="md:text-2xl text-red-500 font-semibold text-center text-base relative z-[2]">
+					Some error occurred. Could not load posts. Please try again
+					later.
+				</h3>
+				<img
+					src={errorImage}
+					alt="Broken error image"
+					className="mx-auto w-full h-full mt-[-7rem] md:mt-[-4rem]"
+				/>
+			</div>
+		</section>
+	) : (
+		<section className="home p-0 md:px-8 border-0 md:border-l lg:border-r border-x-sky-400 flex flex-col items-center justify-start  w-full">
+			<div className="max-w-[1080px] w-full flex flex-col justify-start items-center">
+				{unfollowedUsersPosts.length ? (
+					<PostsList posts={unfollowedUsersPosts} />
+				) : (
+					<>
+						<h2 className="mt-7 md:text-3xl text-green-600 font-semibold text-center text-lg relative z-[2]">
+							You are all caught up!
+						</h2>
+						<img
+							src={caughtUp}
+							alt="Broken error image"
+							className="mx-auto w-full h-full mt-[-7rem] md:mt-[-4rem]"
+						/>
+					</>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export { Explore };
