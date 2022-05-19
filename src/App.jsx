3616156b85283af6fade80e708903f3d285450b7
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 14:03:13 +0530

    feat: scroll to top on new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer, Navbar } from "components";
 import { NavRoutes } from "routes/NavRoutes";
 import { useLocation } from "react-router-dom";
 
 const App = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location?.pathname]);
+
 	return (
 		<div className="bg-gray-200 min-h-[100vh] flex-col flex justify-center w-full items-center text-slate-900 dark:bg-slate-900 dark:text-gray-100 shadow-xl transition-all ease-in">
 			<NavRoutes />
