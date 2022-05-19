16315423e41fe9c8a9fbb2051579662fce434b56
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 22:11:12 +0530

    feat: displayed sorted posts

--- src/features/Post/components/PostsList/index.jsx
+++ src/features/Post/components/PostsList/index.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { PostItem } from "features";
+import { useSelector } from "react-redux";
+
+import { PostItem, getPostsState } from "features";
 import { useInfiniteScroll } from "hooks";
 import inifnityLoading from "images/loading-infinity.svg";
+import { getSortedPosts } from "utils";
+import { useLocation } from "react-router-dom";
 
 const PostsList = ({ posts }) => {
 	const { pageNumber, loading, lastElementReference, hasMorePosts } =
 		useInfiniteScroll(posts);
+	const location = useLocation();
+	const { sortBy } = useSelector(getPostsState);
 
-	const postsToDisplay = posts.slice(0, pageNumber * 3);
+	const sortedPosts =
+		location.pathname === "/home"
+			? getSortedPosts(posts, sortBy)
+			: getSortedPosts(posts, "LATEST");
 
+	const postsToDisplay = sortedPosts?.slice(0, pageNumber * 3);
 	return (
 		<div className="w-full flex justify-center items-center gap-8 my-12 flex-col">
-			{postsToDisplay.map((post) => (
+			{postsToDisplay?.map((post) => (
 				<PostItem key={post._id} post={post} />
 			))}
 			<div key="last-element" ref={lastElementReference}>
-				{hasMorePosts && loading && (
+				{postsToDisplay?.length && hasMorePosts && loading && (
 					<img
 						src={inifnityLoading}
 						className="w-20 h-20"
