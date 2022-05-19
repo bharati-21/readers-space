126f28d77599f7baa9c47b4d7d01fc691e31fcff
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 17:55:41 +0530

    feat: added hook for infinite scrolling

---
+++ src/hooks/useInfiniteScroll.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState, useCallback } from "react";
+
+const useInfiniteScroll = (posts) => {
+	const [pageNumber, setPageNumber] = useState(1);
+	const lastElementReference = useRef(null);
+	const postsLength = posts.length;
+	const hasMorePosts = pageNumber < Math.ceil(postsLength / pageNumber);
+	const [loading, setLoading] = useState(false);
+
+	const handleObserver = (entries) => {
+		const [target] = entries;
+
+		if (!loading && target.isIntersecting && hasMorePosts) {
+			setLoading(true);
+
+			setTimeout(() => {
+				setPageNumber((prevPageNumber) => prevPageNumber + 1);
+				setLoading(false);
+			}, 1000);
+		}
+	};
+
+	useEffect(() => {
+		const reference = lastElementReference.current;
+		const observer = new IntersectionObserver(handleObserver, {
+			threshold: 1,
+		});
+
+		if (reference) observer.observe(reference);
+
+		return () => {
+			if (reference) observer.unobserve(reference);
+		};
+	}, [hasMorePosts, handleObserver]);
+
+	return { loading, lastElementReference, pageNumber, hasMorePosts };
+};
+
+export { useInfiniteScroll };
