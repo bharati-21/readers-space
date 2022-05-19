45361ed9e1c07201789fd8b4cadf960aeb272204
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-19 14:10:24 +0530

    feat: suggested users, follow, unfollow user

--- src/features/Users/usersSlice.js
+++ src/features/Users/usersSlice.js
@@ -1,4 +1,8 @@
-import { getAllUsersService } from "services";
+import {
+	getAllUsersService,
+	postFollowUserService,
+	postUnfollowUserService,
+} from "services";
 
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
@@ -6,14 +10,39 @@ const initialState = {
 	users: [],
 	usersLoading: true,
 	usersError: null,
+	suggestedUsers: [],
 };
 
 export const getAllUsers = createAsyncThunk(
 	"users/getUsers",
+	async (authToken, { rejectWithValue }) => {
+		try {
+			const { data } = await getAllUsersService(authToken);
+			return data.users;
+		} catch (error) {
+			rejectWithValue(error.message);
+		}
+	}
+);
+
+export const postFollowUser = createAsyncThunk(
+	"users/postFollowUser",
 	async ({ authToken, username }, { rejectWithValue }) => {
 		try {
-			const { data } = await getAllUsersService(authToken, username);
-			return data.user;
+			const { data } = await postFollowUserService(authToken, username);
+			return data;
+		} catch (error) {
+			rejectWithValue(error.message);
+		}
+	}
+);
+
+export const postUnfollowUser = createAsyncThunk(
+	"users/postUnfollowUser",
+	async ({ authToken, username }, { rejectWithValue }) => {
+		try {
+			const { data } = await postUnfollowUserService(authToken, username);
+			return data;
 		} catch (error) {
 			rejectWithValue(error.message);
 		}
@@ -23,7 +52,11 @@ export const getAllUsers = createAsyncThunk(
 const usersSlice = createSlice({
 	name: "users",
 	initialState,
-	reducers: {},
+	reducers: {
+		addSuggestedUsers: (state, action) => {
+			state.suggestedUsers = action.payload;
+		},
+	},
 	extraReducers: (builder) => {
 		builder
 			.addCase(getAllUsers.pending, (state) => {
@@ -36,9 +69,46 @@ const usersSlice = createSlice({
 			.addCase(getAllUsers.rejected, (state, action) => {
 				state.usersError = action.payload;
 				state.usersLoading = false;
+			})
+			.addCase(postFollowUser.pending, (state) => {
+				state.usersLoading = true;
+			})
+			.addCase(postFollowUser.fulfilled, (state, action) => {
+				const { user, followUser } = action.payload;
+				state.users = state.users.map((stateUser) =>
+					stateUser.username === user.username
+						? { ...user }
+						: stateUser.username === followUser.username
+						? { ...followUser }
+						: stateUser
+				);
+				state.usersLoading = false;
+			})
+			.addCase(postFollowUser.rejected, (state, action) => {
+				state.usersError = action.payload;
+				state.usersLoading = false;
+			})
+			.addCase(postUnfollowUser.pending, (state) => {
+				state.usersLoading = true;
+			})
+			.addCase(postUnfollowUser.fulfilled, (state, action) => {
+				const { user, followUser } = action.payload;
+				state.users = state.users.map((stateUser) =>
+					stateUser.username === user.username
+						? { ...user }
+						: stateUser.username === followUser.username
+						? { ...followUser }
+						: stateUser
+				);
+				state.usersLoading = false;
+			})
+			.addCase(postUnfollowUser.rejected, (state, action) => {
+				state.usersError = action.payload;
+				state.usersLoading = false;
 			});
 	},
 });
 
 export const getUsersState = (state) => state.users;
 export const usersReducer = usersSlice.reducer;
+export const { addSuggestedUsers } = usersSlice.actions;
