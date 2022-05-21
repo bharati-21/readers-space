543bfa1288a2e90a0a7d0a529bb67f3b2cf58851
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-21 12:41:28 +0530

    fix: memoize debounce() to fix re-render issue

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { getUsersState } from "features";
 import { useSelector } from "react-redux";
 import { getSearchedUsers } from "utils";
@@ -7,31 +7,36 @@ const useDebounce = () => {
 	const [searchLoading, setSearchLoading] = useState(false);
 	const { users } = useSelector(getUsersState);
 	const [searchQueryText, setSearchQueryText] = useState("");
-	let timeout;
 	const [searchedUsers, setSearchedUsers] = useState([]);
 
 	const handleSearchTextChange = (event) => {
 		const searchText = event.target.value;
 		setSearchQueryText(searchText);
-		setSearchedUsers([]);
-		if (searchText !== "") updateSearchedUsers(searchText);
+		if (searchText !== "") {
+			updateSearchedUsers(searchText);
+		}
 	};
 
-	const updateSearchedUsers = debounce((searchText) => {
-		setSearchedUsers(getSearchedUsers(users, searchText));
-	});
-
-	function debounce(callback, delay = 1000) {
+    const debounce = (callback, delay = 1000) => {
+		let timeout;
 		return (...args) => {
 			setSearchLoading(true);
 			clearTimeout(timeout);
-
 			timeout = setTimeout(() => {
 				setSearchLoading(false);
 				callback(...args);
 			}, delay);
 		};
 	}
+
+	const updateSearchedUsers = useCallback(
+		debounce((searchText) => {
+			setSearchedUsers(getSearchedUsers(users, searchText));
+		}),
+		[]
+	);
+
+	
 
 	return {
 		searchLoading,
