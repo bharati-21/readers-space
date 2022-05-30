9466019e74d03fd74d149a98c6d35b306e13519c
Bharati Subramanian <bharatisharada@gmail.com>
2022-05-30 12:58:26 +0530

    refactor: added follow/ unfollow user calls

--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -1,41 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { getAuthState, postFollowUser } from "features";
+import {
+	getAuthState,
+	getModalState,
+	getUsersState,
+	postFollowUser,
+	postUnfollowUser,
+} from "features";
 import { useToast } from "hooks";
+import { getUserDetails } from "utils";
 
-const UsersList = ({ userList, inComponent }) => {
+const UsersList = ({ userList, inComponent, type }) => {
 	const {
 		authUser: { username },
 		authToken,
 	} = useSelector(getAuthState);
+	const { users } = useSelector(getUsersState);
 	const dispatch = useDispatch();
 	const { showToast } = useToast();
 	const navigate = useNavigate();
+	const { onGoingNetworkCall } = useSelector(getUsersState);
 
-	const handleUserFollow = async (event, username) => {
+	const handleUserFollow = async (event, username, isFollowing) => {
 		try {
-			const response = await dispatch(
-				postFollowUser({ authToken, username })
-			);
+			const response = isFollowing
+				? await dispatch(postUnfollowUser({ authToken, username }))
+				: await dispatch(postFollowUser({ authToken, username }));
 			if (response?.error) {
 				throw new Error(
-					"Failed to follow user. Please try again later."
+					`Failed to ${
+						isFollowing ? "unfollow" : "follow"
+					} user. Please try again later.`
 				);
 			}
 			localStorage.setItem(
 				"readers-space-user",
 				JSON.stringify(response.payload.user)
 			);
-			showToast("Followed user successfully.", "success");
+			showToast(
+				`${isFollowing ? "Unfollowed" : "Followed"} user successfully.`,
+				"success"
+			);
 		} catch (error) {
 			showToast(error.message, "error");
 		}
 	};
 
-	const handleUserInfoClicked = (username) => {
+	const handleUserItemClicked = (username) => {
 		if (inComponent === "SEARCH_RESULTS") navigate(`/profile/${username}`);
+		return;
+	};
+
+	const handleUserInfoClicked = (username) => {
+		if (inComponent === "SEARCH_RESULTS") return;
+		navigate(`/profile/${username}`);
 	};
 
 	const mappedUsersList = userList?.map((user) => {
@@ -43,6 +63,13 @@ const UsersList = ({ userList, inComponent }) => {
 			user.username === username
 				? JSON.parse(localStorage.getItem("readers-space-user"))
 				: user;
+
+		const following = getUserDetails(users, username)?.following;
+		const isFollowing = following?.find(
+			(followingUser) => followingUser.username === user.username
+		)
+			? true
+			: false;
 
 		return (
 			<div
@@ -52,7 +79,7 @@ const UsersList = ({ userList, inComponent }) => {
 						? "border-b last:border-b-0 border-b-gray-400 pb-2 px-2 first:pt-2 cursor-pointer w-full"
 						: "border-none"
 				} ${inComponent === "MODAL" ? "p-2" : "p-0"}`}
-				onClick={(e) => handleUserInfoClicked(user.username)}
+				onClick={(e) => handleUserItemClicked(user.username)}
 			>
 				<li className="flex flex-row items-start justify-between w-full">
 					<div className="user-info gap-2 flex flex-row items-start justfiy-between">
@@ -88,10 +115,13 @@ const UsersList = ({ userList, inComponent }) => {
 					</div>
 					{inComponent === "SUGGESTED_USERS" ? (
 						<button
-							className="btn-primary text-xs py-1 px-2"
-							onClick={(e) => handleUserFollow(e, user.username)}
+							className="btn-primary text-xs py-1 px-2 disabled:disabled-btn"
+							onClick={(e) =>
+								handleUserFollow(e, user.username, isFollowing)
+							}
+							disabled={onGoingNetworkCall}
 						>
-							Follow
+							{isFollowing ? "Unfollow" : "Follow"}
 						</button>
 					) : null}
 				</li>
@@ -102,7 +132,7 @@ const UsersList = ({ userList, inComponent }) => {
 		<ul
 			className={`flex list-none flex-col gap-4 justify-start w-full ${
 				inComponent === "SEARCH_RESULTS"
-					? "bg-inherit p-0 border border-gray-400"
+					? "bg-inherit p-0 border border-gray-400  max-h-[250px] overflow-y-auto"
 					: "bg-gray-100 dark:bg-slate-800 p-3"
 			} rounded-sm ${inComponent == "MODAL" ? "py-6" : null}`}
 		>
